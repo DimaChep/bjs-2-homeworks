2df7bee7fa7eb67d065b6853d13b983a3650c214
Дмитрий Чепиницкий <chp-dn@yandex.ru>
2023-04-29 17:58:57 +0300

    Задание готово к проверке

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,5 @@
 function compareArrays(arr1, arr2) {
-  
+  let number = [1, 3, 4, 5];
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+function getUsersNamesInAgeRange(users, gender) {}
