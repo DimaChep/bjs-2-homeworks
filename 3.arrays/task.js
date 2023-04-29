607a9b15429b4c14d32d451e8be8d9561d64bb60
Дмитрий Чепиницкий <chp-dn@yandex.ru>
2023-04-29 18:01:38 +0300

    Готово к проверке

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,27 @@
 function compareArrays(arr1, arr2) {
-  let number = [1, 3, 4, 5];
+  return arr1.every(
+    (element, index) => arr1.length === arr2.length && element === arr2[index]
+  );
 }
 
-function getUsersNamesInAgeRange(users, gender) {}
+function getUsersNamesInAgeRange(users, gender) {
+  if (users.length === 0 || (gender !== "мужской" && gender !== "женский")) {
+    return 0;
+  } else if (gender === "женский") {
+    result =
+      users
+        .filter((user) => user.gender === "женский")
+        .reduce((acc, cur) => acc + cur.age, 0) /
+      users.filter((user) => user.gender === "женский").map((ages) => ages.age)
+        .length;
+    return result;
+  } else if (gender === "мужской") {
+    result =
+      users
+        .filter((user) => user.gender === "мужской")
+        .reduce((acc, cur) => acc + cur.age, 0) /
+      users.filter((user) => user.gender === "мужской").map((ages) => ages.age)
+        .length;
+    return result;
+  }
+}
