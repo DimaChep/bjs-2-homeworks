4a32e3ee33f80a9c3121a25d25ca3b88089e6637
Дмитрий Чепиницкий <chp-dn@yandex.ru>
2023-05-08 19:04:59 +0300

    Поправил замечания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,22 @@
+// Задание 1. Поправил
 function compareArrays(arr1, arr2) {
-  return arr1.every(
-    (element, index) => arr1.length === arr2.length && element === arr2[index]
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
   );
 }
 
+// Задание 2. Поправил
+function getUsersNamesInAgeRange(users, gender) {
+  let result = users
+    .filter((user) => user.gender === gender)
+    .reduce(
+      (averAge, userAge, index, array) => averAge + userAge.age / array.length,
+      0
+    );
+  return result;
+}
+/*
 function getUsersNamesInAgeRange(users, gender) {
   if (users.length === 0 || (gender !== "мужской" && gender !== "женский")) {
     return 0;
@@ -24,4 +37,4 @@ function getUsersNamesInAgeRange(users, gender) {
         .length;
     return result;
   }
-}
+}*/
